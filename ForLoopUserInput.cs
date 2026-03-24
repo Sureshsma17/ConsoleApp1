d8f0fe3d648d827a9292b2ae77576c2282b65893
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-03-24 17:08:56 +0530

    Made changes

---
+++ ForLoopUserInput.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp1
+//{
+//    internal class ForUser_nput
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter Begining Value");
+//            int a = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Enter Ending Value");
+//            int b = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Generating the numbers");
+//            for(int i=a;i<=b;i++)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//    }
+//}
